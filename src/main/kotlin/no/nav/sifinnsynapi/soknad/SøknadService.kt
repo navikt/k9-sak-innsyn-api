21f5e53a70283442bfe0a8e8b6832aec35820e95
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-09-10 09:06:24 +0200

    Legger til testdata for testing i dev.

--- src/main/kotlin/no/nav/sifinnsynapi/soknad/SøknadService.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/soknad/SøknadService.kt
@@ -1,14 +1,34 @@
 package no.nav.sifinnsynapi.soknad
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import no.nav.k9.søknad.Søknad
+import no.nav.k9.søknad.felles.Versjon
+import no.nav.k9.søknad.felles.personopplysninger.Barn
+import no.nav.k9.søknad.felles.personopplysninger.Søker
+import no.nav.k9.søknad.felles.type.Journalpost
+import no.nav.k9.søknad.felles.type.NorskIdentitetsnummer
+import no.nav.k9.søknad.felles.type.Periode
+import no.nav.k9.søknad.felles.type.Språk
+import no.nav.k9.søknad.ytelse.psb.v1.*
+import no.nav.k9.søknad.ytelse.psb.v1.Beredskap.BeredskapPeriodeInfo
+import no.nav.k9.søknad.ytelse.psb.v1.LovbestemtFerie.LovbestemtFeriePeriodeInfo
+import no.nav.k9.søknad.ytelse.psb.v1.Nattevåk.NattevåkPeriodeInfo
+import no.nav.k9.søknad.ytelse.psb.v1.arbeidstid.Arbeidstaker
+import no.nav.k9.søknad.ytelse.psb.v1.arbeidstid.Arbeidstid
+import no.nav.k9.søknad.ytelse.psb.v1.tilsyn.TilsynPeriodeInfo
+import no.nav.k9.søknad.ytelse.psb.v1.tilsyn.Tilsynsordning
 import no.nav.security.token.support.spring.SpringTokenValidationContextHolder
-import no.nav.sifinnsynapi.common.PersonIdentifikator
 import no.nav.sifinnsynapi.http.SøknadNotFoundException
-import no.nav.sifinnsynapi.util.TokenClaims.CLAIM_PID
-import no.nav.sifinnsynapi.util.TokenClaims.CLAIM_SUB
+import no.nav.sifinnsynapi.util.komplettYtelse
 import no.nav.sifinnsynapi.util.personIdent
 import org.springframework.stereotype.Service
+import java.time.LocalDate
+import java.time.ZonedDateTime
 import java.util.*
+import java.util.Map
+import kotlin.collections.List
+import kotlin.collections.listOf
+import kotlin.collections.map
 
 
 @Service
@@ -39,5 +59,28 @@ class SøknadService(
         behandlingsdato = behandlingsdato,
         søknad = søknad
     )
+
+    fun hentTestData(): List<SøknadDTO> {
+        val søknadId = UUID.randomUUID()
+
+        return listOf(
+            SøknadDTO(
+                søknadId = søknadId,
+                søknad = Søknad()
+                    .medSøknadId(søknadId.toString())
+                    .medSøker(Søker(NorskIdentitetsnummer.of(tokenValidationContextHolder.personIdent().personIdent)))
+                    .medJournalpost(Journalpost().medJournalpostId("123456789"))
+                    .medSpråk(Språk.NORSK_BOKMÅL)
+                    .medMottattDato(ZonedDateTime.now())
+                    .medVersjon(Versjon.of("1.0"))
+                    .medYtelse(
+                        komplettYtelse(Periode(LocalDate.now().minusWeeks(1), LocalDate.now().plusWeeks(1)))
+                    ),
+                opprettet = null,
+                endret = null,
+                behandlingsdato = null
+            )
+        )
+    }
 }
 
