5f2a07f2a8bf6b59f8c04a06eef8859d5c8e2751
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-11-19 13:43:02 +0100

    Legger til konsumering og persistering av innsynhendelser.

--- src/main/kotlin/no/nav/sifinnsynapi/soknad/SøknadRepository.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/soknad/SøknadRepository.kt
@@ -1,14 +1,28 @@
 package no.nav.sifinnsynapi.soknad
 
-import no.nav.sifinnsynapi.common.PersonIdentifikator
 import no.nav.sifinnsynapi.config.TxConfiguration.Companion.TRANSACTION_MANAGER
+import org.hibernate.annotations.QueryHints.READ_ONLY
+import org.hibernate.jpa.QueryHints.HINT_CACHEABLE
+import org.hibernate.jpa.QueryHints.HINT_FETCH_SIZE
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.jpa.repository.QueryHints
 import org.springframework.transaction.annotation.Transactional
-import java.util.*
+import java.util.stream.Stream
+import javax.persistence.QueryHint
 
 @Transactional(TRANSACTION_MANAGER)
-interface SøknadRepository : JpaRepository<SøknadDAO, UUID> {
-    fun existsBySøknadId(søknadId: UUID): Boolean
-    fun findAllByPersonIdent(personIdent: PersonIdentifikator): List<SøknadDAO>
-    fun findBySøknadId(søknadId: UUID): SøknadDAO?
+interface SøknadRepository : JpaRepository<PsbSøknadDAO, String> {
+
+    @QueryHints(
+        value = [
+            QueryHint(name = HINT_FETCH_SIZE, value = "10"),
+            QueryHint(name = HINT_CACHEABLE, value = "false"),
+            QueryHint(name = READ_ONLY, value = "true")
+        ]
+    )
+    @Query(
+        value = "SELECT s FROM psb_søknad AS s"
+    )
+    fun hentAlleSøknader(): Stream<PsbSøknadDAO>
 }
