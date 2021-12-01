c531c5e314a21fd80912958a3227893085f3dfcd
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-12-01 09:47:17 +0100

    Tester at harOmsorgen fungerer som forventet.
    - Legger til en table for omsorg i initell sql script.

--- src/main/resources/db/migration/V1__initial.sql
+++ src/main/resources/db/migration/V1__initial.sql
@@ -6,4 +6,14 @@ CREATE TABLE psb_søknad
     søknad                  jsonb,
     opprettet_dato          TIMESTAMP               NOT NULL,
     oppdatert_dato          TIMESTAMP               NOT NULL
-)
+);
+
+CREATE TABLE omsorg
+(
+    id                      VARCHAR(50) PRIMARY KEY NOT NULL,
+    søker_aktør_id          VARCHAR(20)             NOT NULL,
+    pleietrengende_aktør_id VARCHAR(20)             NOT NULL,
+    har_omsorgen            BOOLEAN                 NOT NULL,
+    opprettet_dato          TIMESTAMP               NOT NULL,
+    oppdatert_dato          TIMESTAMP               NOT NULL
+);
---
+++ src/test/kotlin/no/nav/sifinnsynapi/omsorg/OmsorgServiceTest.kt
@@ -0,0 +1,85 @@
+package no.nav.sifinnsynapi.omsorg
+
+import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import java.time.ZoneOffset.UTC
+import java.time.ZonedDateTime
+
+@ExtendWith(SpringExtension::class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DirtiesContext
+@ActiveProfiles("test")
+@EnableMockOAuth2Server // Tilgjengliggjør en oicd-provider for test.
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK) // Integrasjonstest - Kjører opp hele Spring Context med alle konfigurerte beans.
+internal class OmsorgServiceTest {
+
+    @Autowired
+    lateinit var omsorgService: OmsorgService
+
+    @Autowired
+    lateinit var omsorgRepository: OmsorgRepository
+
+    @BeforeEach
+    fun setUp() {
+        omsorgRepository.deleteAll()
+    }
+
+    @AfterAll
+    internal fun tearDown() {
+        omsorgRepository.deleteAll()
+    }
+
+    @Test
+    internal fun `gitt at søker har omsorg for pleiepetrengende, forvent true`() {
+        val søkerAktørId = "11111111111"
+        val pleietrengendeAktørId = "22222222222"
+        omsorgRepository.save(
+            OmsorgDAO(
+                id = "1",
+                søkerAktørId = søkerAktørId,
+                pleietrengendeAktørId = pleietrengendeAktørId,
+                harOmsorgen = true,
+                opprettetDato = ZonedDateTime.now(UTC),
+                oppdatertDato = ZonedDateTime.now(UTC)
+            )
+        )
+
+        assertTrue {
+            omsorgService.harOmsorgen(
+                søkerAktørId = søkerAktørId,
+                pleietrengendeAktørId = pleietrengendeAktørId
+            )
+        }
+    }
+
+    @Test
+    internal fun `gitt at søker ikke har omsorg for pleiepetrengende, forvent false`() {
+        val søkerAktørId = "11111111111"
+        val pleietrengendeAktørId = "22222222222"
+        omsorgRepository.save(
+            OmsorgDAO(
+                id = "1",
+                søkerAktørId = søkerAktørId,
+                pleietrengendeAktørId = pleietrengendeAktørId,
+                harOmsorgen = false,
+                opprettetDato = ZonedDateTime.now(UTC),
+                oppdatertDato = ZonedDateTime.now(UTC)
+            )
+        )
+
+        assertFalse {
+            omsorgService.harOmsorgen(
+                søkerAktørId = søkerAktørId,
+                pleietrengendeAktørId = pleietrengendeAktørId
+            )
+        }
+    }
+}
