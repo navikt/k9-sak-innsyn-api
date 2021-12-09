f9b12f62e07579fb5dbd873bdfc9acb8e4aedffc
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2021-12-09 09:35:04 +0100

    Fjerner UpdateTimestamp på felt og setter default now()

--- src/main/kotlin/no/nav/sifinnsynapi/omsorg/OmsorgDAO.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/omsorg/OmsorgDAO.kt
@@ -1,7 +1,7 @@
 package no.nav.sifinnsynapi.omsorg
 
-import org.hibernate.annotations.UpdateTimestamp
 import org.springframework.data.annotation.CreatedDate
+import java.time.ZoneOffset
 import java.time.ZonedDateTime
 import javax.persistence.Column
 import javax.persistence.Entity
@@ -13,6 +13,6 @@ data class OmsorgDAO(
     @Column(name = "søker_aktør_id") val søkerAktørId: String,
     @Column(name = "pleietrengende_aktør_id") val pleietrengendeAktørId: String,
     @Column(name = "har_omsorgen") val harOmsorgen: Boolean,
-    @Column(name = "opprettet_dato") @CreatedDate val opprettetDato: ZonedDateTime? = null,
-    @Column(name = "oppdatert_dato") @UpdateTimestamp val oppdatertDato: ZonedDateTime? = null
+    @Column(name = "opprettet_dato") @CreatedDate val opprettetDato: ZonedDateTime = ZonedDateTime.now(ZoneOffset.UTC),
+    @Column(name = "oppdatert_dato") val oppdatertDato: ZonedDateTime = ZonedDateTime.now(ZoneOffset.UTC)
 )
