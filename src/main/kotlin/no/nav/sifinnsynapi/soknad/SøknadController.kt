5ce4bb65eb81873c53c072fb2dcd74fbecb5e9c7
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2022-12-15 13:16:15 +0100

    Refaktorerer sikkerhetsoppsett.

--- src/main/kotlin/no/nav/sifinnsynapi/soknad/SøknadController.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/soknad/SøknadController.kt
@@ -1,9 +1,10 @@
 package no.nav.sifinnsynapi.soknad
 
-import no.nav.k9.søknad.Søknad
 import no.nav.security.token.support.core.api.Protected
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.security.token.support.core.api.RequiredIssuers
 import no.nav.sifinnsynapi.Routes.SØKNAD
+import no.nav.sifinnsynapi.config.Issuers
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus.OK
 import org.springframework.http.MediaType
@@ -12,7 +13,10 @@ import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@ProtectedWithClaims(issuer = "selvbetjening", claimMap = ["acr=Level4"])
+@RequiredIssuers(
+    ProtectedWithClaims(issuer = Issuers.ID_PORTEN, claimMap = ["acr=Level4"]),
+    ProtectedWithClaims(issuer = Issuers.TOKEN_X, claimMap = ["acr=Level4"])
+)
 class SøknadController(
     private val søknadService: SøknadService
 ) {
