4dbef33436337cf7760218b7823072c21b5485e2
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-02-05 15:29:52 +0100

    Prøver flyway repair.

--- src/main/kotlin/no/nav/sifinnsynapi/K9SakInnsynApiApplication.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/K9SakInnsynApiApplication.kt
@@ -1,10 +1,12 @@
 package no.nav.sifinnsynapi
 
+import org.flywaydb.core.Flyway
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.retry.annotation.EnableRetry
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -23,4 +25,12 @@ class SifInnsynApiApplication
 
 fun main(args: Array<String>) {
     runApplication<SifInnsynApiApplication>(*args)
+}
+
+// TODO: Fjern etter reparasjon av flyway
+@Configuration
+class FlywayRepairConfig(private val flyway: Flyway) {
+    init {
+        flyway.migrate()
+    }
 }
