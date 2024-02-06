f993ad55e8c7ac901ec9283bd8b3b30fd8717061
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-02-06 12:45:30 +0100

    Dropp behandling tabell.

--- src/main/resources/db/migration/V2__behandling.sql
+++ src/main/resources/db/migration/V2__behandling.sql
@@ -1,11 +1,1 @@
-CREATE TABLE IF NOT EXISTS behandling
-(
-    behandling_id           VARCHAR(50) PRIMARY KEY NOT NULL,
-    søker_aktør_id          VARCHAR(20)             NOT NULL,
-    pleietrengende_aktør_id VARCHAR(20)             NOT NULL,
-    ytelsetype_kode         VARCHAR(8)              NOT NULL,
-    saksnummer              VARCHAR(20)             NOT NULL,
-    behandling              jsonb                   NOT NULL,
-    opprettet_dato          TIMESTAMP               NOT NULL,
-    oppdatert_dato          TIMESTAMP               NOT NULL
-);
+DROP TABLE IF EXISTS behandling;
