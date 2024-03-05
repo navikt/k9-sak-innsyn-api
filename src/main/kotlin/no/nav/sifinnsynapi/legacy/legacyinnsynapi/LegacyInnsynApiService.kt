8f28d740dfad984fea8801af1f4e2686bdf9135a
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-03-05 09:06:06 +0100

    Skal ikke retry dersom søknad ikke blir funnet.

--- src/main/kotlin/no/nav/sifinnsynapi/legacy/legacyinnsynapi/LegacyInnsynApiService.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/legacy/legacyinnsynapi/LegacyInnsynApiService.kt
@@ -16,7 +16,11 @@ import org.springframework.web.util.UriComponentsBuilder
 
 @Service
 @Retryable(
-    exclude = [HttpClientErrorException.Unauthorized::class, HttpClientErrorException.Forbidden::class, ResourceAccessException::class],
+    exclude = [
+        HttpClientErrorException.Unauthorized::class,
+        HttpClientErrorException.Forbidden::class,
+        HttpClientErrorException.NotFound::class,
+        ResourceAccessException::class],
     backoff = Backoff(
         delayExpression = "\${spring.rest.retry.initialDelay}",
         multiplierExpression = "\${spring.rest.retry.multiplier}",
