b05385eddf2a749266859994735b8bfc706e6c50
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-04-22 12:17:16 +0200

    Logger dersom systemoppslag av barn returnerer tom resultat

--- src/test/kotlin/no/nav/sifinnsynapi/sak/SakServiceITest.kt
+++ src/test/kotlin/no/nav/sifinnsynapi/sak/SakServiceITest.kt
@@ -9,10 +9,9 @@ import no.nav.k9.innsyn.sak.BehandlingResultat
 import no.nav.k9.innsyn.sak.BehandlingStatus
 import no.nav.k9.innsyn.sak.Fagsak
 import no.nav.k9.innsyn.sak.FagsakYtelseType
-import no.nav.k9.innsyn.sak.InnsendingInfo
-import no.nav.k9.innsyn.sak.InnsendingStatus
-import no.nav.k9.innsyn.sak.InnsendingType
 import no.nav.k9.innsyn.sak.Saksnummer
+import no.nav.k9.innsyn.sak.SøknadInfo
+import no.nav.k9.innsyn.sak.SøknadStatus
 import no.nav.k9.søknad.JsonUtils
 import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
 import no.nav.sifinnsynapi.dokumentoversikt.DokumentService
@@ -164,7 +163,7 @@ class SakServiceITest {
             )
         )
 
-        every { behandlingService.hentBehandlinger(any(), any()) } answers {
+        every { behandlingService.hentBehandlinger(any(), any(), any()) } answers {
             listOf(
                 BehandlingDAO(
                     behandlingId = UUID.randomUUID(),
@@ -172,7 +171,7 @@ class SakServiceITest {
                     pleietrengendeAktørId = barn1AktørId,
                     saksnummer = saksnummer,
                     behandling = JsonUtils.toString(lagBehandling(setOf(
-                        InnsendingInfo(InnsendingStatus.MOTTATT, journalpostId, ZonedDateTime.now(), null, InnsendingType.SØKNAD)
+                        SøknadInfo(SøknadStatus.MOTTATT, journalpostId, ZonedDateTime.now(), null)
                     ))),
                     ytelsetype = FagsakYtelseType.PLEIEPENGER_SYKT_BARN
                 )
@@ -182,68 +181,6 @@ class SakServiceITest {
         val hentSaker = sakService.hentSaker(FagsakYtelseType.PLEIEPENGER_SYKT_BARN)
         Assertions.assertThat(hentSaker).isNotEmpty
         Assertions.assertThat(hentSaker).size().isEqualTo(1)
-    }
-
-    @Test
-    fun `gitt søker ikke har omsorgen for barnet, forvent innsyn i sak og at barnets navn anonymiseres`() {
-        omsorgRepository.oppdaterOmsorg(false, hovedSøkerAktørId, barn1AktørId)
-        omsorgRepository.oppdaterOmsorg(false, hovedSøkerAktørId, barn2AktørId)
-
-        every { oppslagsService.systemoppslagBarn(any()) } returns listOf(
-            BarnOppslagDTO(
-                aktørId = barn1AktørId,
-                fødselsdato = LocalDate.parse("2005-02-12"),
-                fornavn = "Ole",
-                mellomnavn = null,
-                etternavn = "Doffen",
-                identitetsnummer = "12020567099"
-            )
-        )
-
-        val saksnummer = "sak1234"
-        val journalpostId = "123456789"
-
-        every { dokumentService.hentDokumentOversikt() } returns listOf(
-            DokumentDTO(
-                journalpostId = journalpostId,
-                dokumentInfoId = "123456789",
-                saksnummer = Saksnummer(saksnummer),
-                tittel = "Søknad om pleiepenger til sykt barn",
-                dokumentType = DokumentBrevkode.PLEIEPENGER_SYKT_BARN_SOKNAD,
-                filtype = "PDFA",
-                harTilgang = true,
-                url = URL("http://localhost:8080/saker/123456789"),
-                journalposttype = Journalposttype.INNGÅENDE,
-                relevanteDatoer = listOf(
-                    RelevantDatoDTO(
-                        dato = LocalDate.now().toString(),
-                        datotype = Datotype.DATO_OPPRETTET
-                    )
-                )
-            )
-        )
-
-        every { behandlingService.hentBehandlinger(any(), any()) } answers {
-            listOf(
-                BehandlingDAO(
-                    behandlingId = UUID.randomUUID(),
-                    søkerAktørId = hovedSøkerAktørId,
-                    pleietrengendeAktørId = barn1AktørId,
-                    saksnummer = saksnummer,
-                    behandling = JsonUtils.toString(lagBehandling(setOf(
-                        InnsendingInfo(InnsendingStatus.MOTTATT, journalpostId, ZonedDateTime.now(), null, InnsendingType.SØKNAD)
-                    ))),
-                    ytelsetype = FagsakYtelseType.PLEIEPENGER_SYKT_BARN
-                )
-            ).stream()
-        }
-
-        val hentSaker = sakService.hentSaker(FagsakYtelseType.PLEIEPENGER_SYKT_BARN)
-        Assertions.assertThat(hentSaker).isNotEmpty
-        Assertions.assertThat(hentSaker).size().isEqualTo(1)
-        Assertions.assertThat(hentSaker.first().pleietrengende.fornavn).isNull()
-        Assertions.assertThat(hentSaker.first().pleietrengende.mellomnavn).isNull()
-        Assertions.assertThat(hentSaker.first().pleietrengende.etternavn).isNull()
     }
 
     @Test
@@ -289,7 +226,7 @@ class SakServiceITest {
         Assertions.assertThat(saksbehandlingstid.saksbehandlingstidUker).isEqualTo(6)
     }
 
-    private fun lagBehandling(søknadInfos: Set<InnsendingInfo>): Behandling {
+    private fun lagBehandling(søknadInfos: Set<SøknadInfo>): Behandling {
         return Behandling(
             UUID.randomUUID(),
             ZonedDateTime.now(),
