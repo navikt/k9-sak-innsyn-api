a3168de3ed9dc8a3af897fad4a0dd7d11da9e777
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2024-05-23 13:42:52 +0200

    Fikser annotasjon

--- src/main/kotlin/no/nav/sifinnsynapi/common/ProfilAnnotasjoner.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/common/ProfilAnnotasjoner.kt
@@ -6,5 +6,5 @@ import org.springframework.context.annotation.Profile
  * Markerer at en komponent ikke er aktivert i produksjon.
  * Dersom en klasse eller en metode er annotert med denne, vil det ikke bli opprettet en bean av denne i produksjonsprofilen.
  */
-@Profile("!prod")
+@Profile("!prod-gcp")
 annotation class IkkeAktivertIProduksjon
