888d95d1090fb0099edd12a8d6e0f62ec4aa802b
Qadeer Ahmad Khan <qadeer.ahmad.khan@nav.no>
2024-08-29 13:45:44 +0200

    Oppdaterer tester med ny frist

--- src/test/kotlin/no/nav/sifinnsynapi/sak/behandling/SaksbehandlingstidUtlederTest.kt
+++ src/test/kotlin/no/nav/sifinnsynapi/sak/behandling/SaksbehandlingstidUtlederTest.kt
@@ -25,7 +25,7 @@ class SaksbehandlingstidUtlederTest {
         )
         val saksbehandlingsfrist = SaksbehandlingstidUtleder.utled(behandling,(null))
 
-        assertThat(saksbehandlingsfrist).isEqualTo(tidligsteMottattTidspunkt.plusWeeks(6))
+        assertThat(saksbehandlingsfrist).isEqualTo(tidligsteMottattTidspunkt.plusWeeks(4))
     }
 
     @Test
@@ -55,7 +55,7 @@ class SaksbehandlingstidUtlederTest {
             lagSøknad(tidligsteMottattTidspunkt.plusMonths(20), Kildesystem.SØKNADSDIALOG)
         ))
         val saksbehandlingsfrist = SaksbehandlingstidUtleder.utled(behandling)
-        assertThat(saksbehandlingsfrist).isEqualTo(tidligsteSøknadsTidspunkt.plusWeeks(6))
+        assertThat(saksbehandlingsfrist).isEqualTo(tidligsteSøknadsTidspunkt.plusWeeks(4))
 
     }
 
