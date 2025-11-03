d7433a4bfc9c6ccaa9185bd9cf966a5b9188126c
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-11-03 11:09:28 +0100

    Legger til im status.

--- src/main/kotlin/no/nav/sifinnsynapi/sak/inntektsmelding/InntektsmeldingService.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/sak/inntektsmelding/InntektsmeldingService.kt
@@ -2,14 +2,13 @@ package no.nav.sifinnsynapi.sak.inntektsmelding
 
 import no.nav.k9.innsyn.inntektsmelding.Arbeidsgiver
 import no.nav.k9.innsyn.inntektsmelding.Inntektsmelding
+import no.nav.k9.innsyn.inntektsmelding.InntektsmeldingStatus
 import no.nav.k9.innsyn.sak.Saksnummer
 import no.nav.k9.søknad.JsonUtils
 import no.nav.sifinnsynapi.enhetsregisteret.EnhetsregisterService
 import no.nav.sifinnsynapi.oppslag.OppslagsService
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
-import java.util.stream.Stream
-import kotlin.streams.toList
 
 @Service
 class InntektsmeldingService(
@@ -42,6 +41,7 @@ class InntektsmeldingService(
         val im = JsonUtils.fromString(inntektsmelding, Inntektsmelding::class.java)
 
         return SakInntektsmeldingDTO(
+            status = im.status.somInntektsmeldingStatusDto(),
             saksnummer = saksnummer,
             journalpostId = journalpostId,
             arbeidsgiver = im.arbeidsgiver.somArbeidsgiverDTO(),
@@ -52,6 +52,13 @@ class InntektsmeldingService(
             kildesystem = im.kildesystem,
             erstattetAv = im.erstattetAv.map { it.journalpostId }
         )
+    }
+
+    private fun InntektsmeldingStatus.somInntektsmeldingStatusDto(): InntektsmeldingStatusDto = when (this) {
+        InntektsmeldingStatus.I_BRUK -> InntektsmeldingStatusDto.I_BRUK
+        InntektsmeldingStatus.MANGLER_DATO -> InntektsmeldingStatusDto.MANGLER_DATO
+        InntektsmeldingStatus.IKKE_RELEVANT -> InntektsmeldingStatusDto.IKKE_RELEVANT
+        InntektsmeldingStatus.ERSTATTET_AV_NYERE -> InntektsmeldingStatusDto.ERSTATTET_AV_NYERE
     }
 
     private fun Arbeidsgiver.somArbeidsgiverDTO(): ArbeidsgiverDTO {
--- src/main/kotlin/no/nav/sifinnsynapi/sak/inntektsmelding/SakInntektsmeldingDTO.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/sak/inntektsmelding/SakInntektsmeldingDTO.kt
@@ -5,10 +5,10 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 
 data class SakInntektsmeldingDTO(
+    val status: InntektsmeldingStatusDto,
     val saksnummer: String,
     val journalpostId: String,
     val arbeidsgiver: ArbeidsgiverDTO,
-
     val startDatoPermisjon: LocalDate?,
     val mottattDato: LocalDate,
     val inntektBeløp: BigDecimal,
