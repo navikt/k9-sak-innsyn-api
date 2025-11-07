deb793f3ab0b047f774b2485afa969e04fc59ce6
Ramin Esfandiari <ramin.esfandiari@nav.no>
2025-11-07 10:20:51 +0100

    Legger til RefusjonDTO for å håndtere refusjonsinformasjon i inntektsmeldinger

---
+++ src/main/kotlin/no/nav/sifinnsynapi/sak/inntektsmelding/typer/RefusjonDTO.kt
@@ -0,0 +1,23 @@
+package no.nav.sifinnsynapi.sak.inntektsmelding.typer
+
+import no.nav.k9.søknad.TidUtils.TIDENES_ENDE
+import java.math.BigDecimal
+import java.time.LocalDate
+
+data class RefusjonDTO(
+    val refusjonBeløpPerMnd: BigDecimal,
+    val refusjonOpphører: LocalDate?,
+) {
+    val utledetRefusjon: UtledetRefusjonType
+        get() {
+            return when (val dato: LocalDate? = refusjonOpphører) {
+                null, TIDENES_ENDE -> UtledetRefusjonType.OPPHØRER_ALDRI
+                else -> UtledetRefusjonType.OPPHØRER
+            }
+        }
+}
+
+enum class UtledetRefusjonType {
+    OPPHØRER,
+    OPPHØRER_ALDRI
+}
