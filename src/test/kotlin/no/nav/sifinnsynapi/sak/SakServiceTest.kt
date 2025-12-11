f8607e725628b613e4b92c5ac0857e80b8a3e3ee
Anders Østby <anders.ostby@nav.no>
2025-12-11 09:09:11 +0100

    Endrer forventet saksbehandlingstid til 7 uker

--- src/test/kotlin/no/nav/sifinnsynapi/sak/SakServiceTest.kt
+++ src/test/kotlin/no/nav/sifinnsynapi/sak/SakServiceTest.kt
@@ -543,7 +543,7 @@ class SakServiceTest {
     @Test
     fun `Forvent saksbehandlingstid oppgitt i uker`() {
         val saksbehandlingstid = sakService.hentGenerellSaksbehandlingstid()
-        Assertions.assertThat(saksbehandlingstid.saksbehandlingstidUker).isEqualTo(6)
+        Assertions.assertThat(saksbehandlingstid.saksbehandlingstidUker).isEqualTo(7)
     }
 
     private fun lagDokumentDto(journalpostId: String) = DokumentDTO(
