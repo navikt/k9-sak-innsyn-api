cbae553395884f1f07a2796f31126bcce33e70bc
Anders Østby <anders.ostby@nav.no>
2025-12-11 09:35:01 +0100

    Bytter ut deprecated URL-constructor med URI(..).toURL()

--- src/test/kotlin/no/nav/sifinnsynapi/sak/SakServiceTest.kt
+++ src/test/kotlin/no/nav/sifinnsynapi/sak/SakServiceTest.kt
@@ -33,7 +33,7 @@ import no.nav.sifinnsynapi.soknad.InnsendingService
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import java.net.URL
+import java.net.URI
 import java.time.LocalDate
 import java.time.ZoneId
 import java.time.ZonedDateTime
@@ -556,7 +556,7 @@ class SakServiceTest {
         harTilgang = true,
         journalposttype = Journalposttype.INNGÅENDE,
         relevanteDatoer = emptyList(),
-        url = URL("http://localhost:8080/dokument/journalpostId1/dokumentInfo1/ARKIV")
+        url = URI("http://localhost:8080/dokument/journalpostId1/dokumentInfo1/ARKIV").toURL()
     )
 
     private fun lagPsbSøknad(journalpostId: String) = PsbSøknadDAO(
