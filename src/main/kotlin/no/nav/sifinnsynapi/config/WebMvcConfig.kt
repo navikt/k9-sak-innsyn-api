802b82ff5d9273d082d7e82f3162cd0f860e2dfb
Qadeer Khan <ahm.qadeer@gmail.com>
2026-02-23 10:03:12 +0100

    jackson3 JsonMapper blir brukt i RestControllere istedenfor ObjectMap… (#479)
    …per fra jackson 2. Det virker som JsonMapper/Customiser kommer
    transitivt også. Legger inn denne configen slik at vi har bedre kontroll
    på dette.

--- src/main/kotlin/no/nav/sifinnsynapi/config/WebMvcConfig.kt
+++ src/main/kotlin/no/nav/sifinnsynapi/config/WebMvcConfig.kt
@@ -5,10 +5,14 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.SerializationFeature
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.boot.jackson.autoconfigure.JsonMapperBuilderCustomizer
 import org.springframework.boot.jackson2.autoconfigure.Jackson2ObjectMapperBuilderCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import tools.jackson.databind.cfg.DateTimeFeature
+import tools.jackson.module.kotlin.KotlinFeature
+import tools.jackson.module.kotlin.KotlinModule
 
 @Configuration
 class WebMvcConfig() : WebMvcConfigurer {
@@ -27,6 +31,23 @@ class WebMvcConfig() : WebMvcConfigurer {
                 SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS
             )
             builder.propertyNamingStrategy(PropertyNamingStrategies.LOWER_CAMEL_CASE)
+        }
+    }
+
+    @Bean
+    fun jackson3BuilderCustomizer(): JsonMapperBuilderCustomizer {
+        log.info("-------> Customizing jackson 3 JsonMapper builder")
+        return JsonMapperBuilderCustomizer { builder ->
+            builder
+                .disable(DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .disable(DateTimeFeature.WRITE_DURATIONS_AS_TIMESTAMPS)
+                .disable(tools.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                // Unngå å prefikse kotlin data klasse felter med prefiks get for non-ascii.
+                // OBS for open api spec så funker ikke dette og feltene må annoteres med @JsonProperty
+                .addModule(
+                    KotlinModule.Builder().enable(KotlinFeature.KotlinPropertyNameAsImplicitName).build()
+                )
+                .propertyNamingStrategy(tools.jackson.databind.PropertyNamingStrategies.LOWER_CAMEL_CASE)
         }
     }
 }
