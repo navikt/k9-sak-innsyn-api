ca59bf8ea1425661db7feb7f56cfe161caf982c0
Vilde <vilde.zcimarzceck@nav.no>
2026-03-27 14:50:24 +0100

    Oppdaterer k9-format versjon (#502)

--- src/test/kotlin/no/nav/sifinnsynapi/sak/SakServiceTest.kt
+++ src/test/kotlin/no/nav/sifinnsynapi/sak/SakServiceTest.kt
@@ -543,7 +543,7 @@ class SakServiceTest {
     @Test
     fun `Forvent saksbehandlingstid oppgitt i uker`() {
         val saksbehandlingstid = sakService.hentGenerellSaksbehandlingstid()
-        Assertions.assertThat(saksbehandlingstid.saksbehandlingstidUker).isEqualTo(11)
+        Assertions.assertThat(saksbehandlingstid.saksbehandlingstidUker).isEqualTo(12)
     }
 
     private fun lagDokumentDto(journalpostId: String) = DokumentDTO(
