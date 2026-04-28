3380cade33e6b32c13a37a0f4b3de4a60bc81497
Morten Osvik <33130876+mortenoosvik@users.noreply.github.com>
2026-04-28 11:38:21 +0200

    Bump org.springframework.boot 4.0.3 -> 4.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "4.0.3"
+    id("org.springframework.boot") version "4.0.5"
     id("io.spring.dependency-management") version "1.1.7"
     id("com.expediagroup.graphql") version "9.1.0"
     kotlin("jvm") version "2.3.20"
