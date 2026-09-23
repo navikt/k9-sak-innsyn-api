acf742f64e5986bc44b95ca0aa9279b941f6b9c9
Aleksander.Andresen <aleksander.andresen@nav.no>
2026-09-23 11:34:50 +0200

    Oppdater test

--- src/test/kotlin/no/nav/sifinnsynapi/k9sak/K9SakServiceTest.kt
+++ src/test/kotlin/no/nav/sifinnsynapi/k9sak/K9SakServiceTest.kt
@@ -1,5 +1,7 @@
 package no.nav.sifinnsynapi.k9sak
 
+import assertk.assertThat
+import assertk.assertions.isEqualTo
 import com.github.tomakehurst.wiremock.client.WireMock
 import com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor
 import com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo
@@ -22,6 +24,7 @@ import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
 import org.wiremock.spring.ConfigureWireMock
 import org.wiremock.spring.EnableWireMock
+import java.time.LocalDate
 
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
@@ -71,11 +74,13 @@ internal class K9SakServiceTest {
             """.trimIndent()
         )
 
-        k9SakService.hentSisteGyldigeVedtakForAktorId(
+        var response = k9SakService.hentSisteGyldigeVedtakForAktorId(
             HentSisteGyldigeVedtakForAktorIdDto(
                 pleietrengendeAktør
             )
         )
+        assertThat(response?.vedtakTomDato).isEqualTo(LocalDate.of(2024, 12, 31))
+        assertThat(response?.førsteSøknadsdato).isEqualTo(LocalDate.of(2024, 10, 1))
     }
 
     @Test
